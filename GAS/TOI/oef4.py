8cf065cbcf984e5d85992ef9e93253ac49a34a28
Duxlol <thijs.vanschel@gmail.com>
2024-11-12 20:07:21 +0100

    TOI 4,5 exercises | needs notes still and half of TOI5

--- GAS/TOI/oef4.py
+++ GAS/TOI/oef4.py
@@ -1,28 +1,32 @@
 class MyQueue:
-    def __init__(self,capacity):
+    def __init__(self, capacity):
         self.capacity = capacity
         self.queue = []
     def isEmpty(self):
-        if len(self.queue) == 0:
-            return True
-        return False
-    def enqueue(self,value):
+        return len(self.queue) == 0
+
+    def enqueue(self, value):
         if len(self.queue) < self.capacity:
             self.queue.append(value)
             return True
         return False
+
     def dequeue(self):
         if self.isEmpty():
             return None, False
-        return self.queue.pop(0),True
+        return self.queue.pop(0), True
+
     def getFront(self):
         if self.isEmpty():
-            return None,False
-        return self.queue[0],True
-    def load(self,nodes):
+            return None, False
+        return self.queue[0], True
+
+    def load(self, nodes):
+        self.queue = []
         if len(nodes) <= self.capacity:
-            self.queue = nodes
-        else:
-            return False
+            self.queue = nodes[::-1]
+            return True
+        return False
+
     def save(self):
-        return self.queue
+        return self.queue[::-1]
---
+++ GAS/TOI/oef5.py
@@ -0,0 +1,125 @@
+class TreeNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def createTreeItem(key, val):
+    return TreeNode(key, val)
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def isEmpty(self):
+        return self.root is None
+
+    def searchTreeInsert(self, item):
+        def insert(node, item):
+            if item.key < node.key:
+                if node.left is None:
+                    node.left = item
+                    return True
+                return insert(node.left, item)
+            elif item.key > node.key:
+                if node.right is None:
+                    node.right = item
+                    return True
+                return insert(node.right, item)
+            return False  # Duplicate key
+
+        if self.isEmpty():
+            self.root = item
+            return True
+        return insert(self.root, item)
+
+    def searchTreeRetrieve(self, key):
+        def retrieve(node, key):
+            if node is None:
+                return None, False
+            if key == node.key:
+                return node.value, True
+            elif key < node.key:
+                return retrieve(node.left, key)
+            else:
+                return retrieve(node.right, key)
+
+        return retrieve(self.root, key)
+
+    def searchTreeDelete(self, key):
+        def delete(node, key):
+            if node is None:
+                return node, False
+            if key < node.key:
+                node.left, deleted = delete(node.left, key)
+            elif key > node.key:
+                node.right, deleted = delete(node.right, key)
+            else:
+                if node.left is None:
+                    return node.right, True
+                elif node.right is None:
+                    return node.left, True
+                min_larger_node = getMin(node.right)
+                node.key, node.value = min_larger_node.key, min_larger_node.value
+                node.right, deleted = delete(node.right, node.key)
+            return node, deleted
+
+        def getMin(node):
+            while node.left is not None:
+                node = node.left
+            return node
+
+        self.root, deleted = delete(self.root, key)
+        return deleted
+
+    def inorderTraverse(self, visit):
+        def inorder(node):
+            if node:
+                inorder(node.left)
+                visit(node.value)
+                inorder(node.right)
+
+        inorder(self.root)
+
+    def save(self):
+        def save_node(node):
+            if node is None:
+                return None
+            node_representation = {'root': node.key}
+            children = [save_node(node.left), save_node(node.right)]
+            if any(children):
+                node_representation['children'] = children
+            return node_representation
+
+        return save_node(self.root)
+
+    def load(self, tree_dict):
+        def load_node(tree_dict):
+            if tree_dict is None:
+                return None
+            node = TreeNode(tree_dict['root'], tree_dict['root'])
+            if 'children' in tree_dict:
+                node.left = load_node(tree_dict['children'][0])
+                node.right = load_node(tree_dict['children'][1])
+            return node
+
+        self.root = load_node(tree_dict)
+if __name__ == "__main__":
+    t = BST()
+    print(t.isEmpty())
+    print(t.searchTreeInsert(createTreeItem(8,8)))
+    print(t.searchTreeInsert(createTreeItem(5,5)))
+    print(t.isEmpty())
+    print(t.searchTreeRetrieve(5)[0])
+    print(t.searchTreeRetrieve(5)[1])
+    t.inorderTraverse(print)
+    print(t.save())
+    t.load({'root': 10,'children':[{'root':5},None]})
+    t.searchTreeInsert(createTreeItem(15,15))
+    print(t.searchTreeDelete(0))
+    print(t.save())
+    print(t.searchTreeDelete(10))
+    print(t.save())
