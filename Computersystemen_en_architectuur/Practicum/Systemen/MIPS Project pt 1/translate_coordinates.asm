fce8cfae14528efaa9847fcc5b3a6f32ff22d752
duxlol <thijs.vanschel@gmail.com>
2024-11-30 16:50:20 +0100

    mips opdr 1

--- Computersystemen_en_architectuur/Practicum/Systemen/MIPS Project pt 1/translate_coordinates.asm
+++ Computersystemen_en_architectuur/Practicum/Systemen/MIPS Project pt 1/translate_coordinates.asm
@@ -10,7 +10,7 @@ offset: .word 128
 msgShowMemoryAddress: .asciiz "The memory address for the pixel is:\n"
 	.text
 main:
-	# rijnummer
+	# row number
 	la $a0, promptRows  	# ask user for the row number
 	li $v0, 4		# syscall print string
     	syscall			# syscall print string
@@ -18,7 +18,7 @@ main:
 	syscall			# syscall read int
 	move $t0, $v0 		# move rownumber to t0
 	
-	# kolomnummer
+	# column number
 	la $a0, promptCols 	# ask user for the column number
 	li $v0, 4		# syscall print string
     	syscall 		# syscall print string
@@ -26,22 +26,22 @@ main:
 	syscall			# syscall read int
 	move $t1, $v0		# move collumn number to t1
 		
-	
-	
-	lw $t3, 0($s0) # rij * offset1
-	mul $t3, $t0, 4 
-	# colums + offset2
-	# mul $t4, $t1, 4
-	lw $s4, 4($t1)
-	
-	# (rij * offset) + (colums + offset)
-	addi $t5, $s4, $s3
+	# berekenen geheugenadres $gp+((row×number of columns)+column)×4
+	li $t2, 32 # load amount of colums in t2
+	mul $t3, $t0, $t2 # calculate rownumber offset
+	add $t4, $t3, $t1 # add column number (t4 = rownumber*#colums + columnnumber)
+	mul $t5, $t4, 4   # byte offset: t5 = t4 * 4
+    	add $t5, $t5, $gp # add $gp
 	
 	# print result
-	#la $t5, $gp
-	#li $v0, 4
-	#syscall
+	la $a0, msgShowMemoryAddress 
+	li $v0, 4 # syscall print string
+	syscall # syscall print string
+	 
+	move $a0, $t5 # move memory address to a0
+	li $v0, 1 # syscall print int
+	syscall # syscall print int
 	
-	
-	
-	
+	# exit
+	li $v0, 10
+	syscall
